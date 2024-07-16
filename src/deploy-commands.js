f61b41ca090c6c4dfa682361a86976b2f3c90404
Justyna Skalska <jastka4@gmail.com>
2024-07-16 16:19:32 +0200

    Make commands global

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -3,45 +3,40 @@ const { REST, Routes } = require("discord.js");
 const fs = require("node:fs");
 const path = require("node:path");
 
-const rest = new REST().setToken(process.env.TOKEN);
+const commands = [];
 
-function getCommands() {
-  const commands = [];
-  const foldersPath = path.join(__dirname, "commands");
-  const commandFolders = fs.readdirSync(foldersPath);
+const foldersPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath);
 
-  for (const folder of commandFolders) {
-    const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs
-      .readdirSync(commandsPath)
-      .filter((file) => file.endsWith(".js"));
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
 
-    for (const file of commandFiles) {
-      const filePath = path.join(commandsPath, file);
-      const command = require(filePath);
-      if ("data" in command && "execute" in command) {
-        commands.push(command.data.toJSON());
-      } else {
-        console.log(
-          `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-        );
-      }
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ("data" in command && "execute" in command) {
+      commands.push(command.data.toJSON());
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
     }
   }
-
-  return commands;
 }
 
-async function deployCommands(guildId) {
-  try {
-    const commands = getCommands();
+const rest = new REST().setToken(process.env.TOKEN);
 
+(async () => {
+  try {
     console.log(
       `Started refreshing ${commands.length} application (/) commands.`
     );
 
     const data = await rest.put(
-      Routes.applicationGuildCommands(process.env.CLIENT_ID, guildId),
+      Routes.applicationCommands(process.env.CLIENT_ID),
       { body: commands }
     );
 
@@ -51,6 +46,4 @@ async function deployCommands(guildId) {
   } catch (error) {
     console.error(error);
   }
-}
-
-module.exports = { deployCommands };
+})();
--- src/events/guildCreate.js
+++
@@ -1,9 +0,0 @@
-const { Events } = require("discord.js");
-const { deployCommands } = require("../deploy-commands");
-
-module.exports = {
-  name: Events.GuildCreate,
-  async execute(guild) {
-    await deployCommands(guild.id);
-  },
-};
