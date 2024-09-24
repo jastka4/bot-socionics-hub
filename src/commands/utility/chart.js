fe885fa17710c9e61768580e671119d5e17c44ed
Justyna Skalska <jastka4@gmail.com>
2024-09-24 20:50:29 +0200

    Add /chart command
    Prints a chart of intertype relationships.

---
+++ src/commands/utility/chart.js
@@ -0,0 +1,44 @@
+const {
+  AttachmentBuilder,
+  EmbedBuilder,
+  SlashCommandBuilder,
+} = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("chart")
+    .setDescription("Prints a chart of intertype relationships."),
+  async execute(interaction) {
+    const file = new AttachmentBuilder("public/media/chart.png");
+    await interaction.reply({
+      embeds: [
+        new EmbedBuilder()
+          .setColor(0xf1c40f)
+          .setTitle("Chart")
+          .addFields({
+            name: ":left_right_arrow: Type to relation mapping",
+            value:
+              "_ _\n" +
+              "**ILE** - identity\n" +
+              "**LIE** - quasi-identity\n" +
+              "**IEE** - kindred\n" +
+              "**EIE** - reverse benefit\n" +
+              "**SLE** - business\n" +
+              "**LSE** - direct benefit\n" +
+              "**SEE** - super-ego\n" +
+              "**ESE** - activation\n" +
+              "**ILI** - extinguishment\n" +
+              "**LII** - mirror\n" +
+              "**IEI** - mirage\n" +
+              "**EII** - direct superevision\n" +
+              "**SLI** - semi-duality\n" +
+              "**LSI** - reverse superevision\n" +
+              "**SEI** - duality\n" +
+              "**ESI** - conflict",
+          })
+          .setImage("attachment://chart.png"),
+      ],
+      files: [file],
+    });
+  },
+};
--- src/commands/utility/help.js
+++ src/commands/utility/help.js
@@ -15,7 +15,8 @@ module.exports = {
               name: ":computer: Commands",
               value:
                 "</calculate:1262564519499137124> - Calculates intertype relationship of provided types.\n" +
-                "</help:1265990525128020020> - Displays the list of available commands.",
+                "</help:1265990525128020020> - Displays the list of available commands.]n" +
+                "</chart:1266106879378194463> - Prints a chart of intertype relationships.",
             },
             {
               name: "_ _",
